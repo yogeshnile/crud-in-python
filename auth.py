c66a5e6135dd5118a95049da41487e85a2b8df58
yogeshnile <yogeshnile786@gmail.com>
2020-08-14 18:04:53 +0530

    directory structure

---
+++ auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/singup')
+def singup():
+    return render_template('singup.html')
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/logout')
+def logout():
+    return "used this to log out"
