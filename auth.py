9f70df9389e7096526c3cede55eecb3497d3bd97
yogeshnile <yogeshnile786@gmail.com>
2020-08-15 15:53:13 +0530

    login, singup ready

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect, request
 
 auth = Blueprint('auth',__name__)
 
@@ -6,9 +6,24 @@ auth = Blueprint('auth',__name__)
 def singup():
     return render_template('singup.html')
 
+@auth.route('/singup', methods=['POST'])
+def singup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    return redirect(url_for('auth.login'))
+
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    return redirect(url_for('main.profile'))
 
 @auth.route('/logout')
 def logout():
