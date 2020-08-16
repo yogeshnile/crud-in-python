ccecb53ad21d85dea160a741c1fde481529d5645
yogeshnile <yogeshnile786@gmail.com>
2020-08-16 17:29:48 +0530

    workout add function added

--- main.py
+++ main.py
@@ -7,8 +7,11 @@
 #  GitHub   : github.com/yogeshnile         #
 #############################################
 
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect, request, flash, abort
 from flask_login import current_user, login_required
+from . import db
+from .models import User
+from .models import Workout
 
 #On this file all basic opration done like maitain profile, pushup etc
 
@@ -22,3 +25,28 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name = current_user.name)
+
+@main.route('/new')
+@login_required
+def new_workout():
+    return render_template('create_workout.html')
+
+@main.route('/new', methods=['POST'])
+@login_required
+def new_workout_post():
+    pushups = request.form.get('pushups')
+    comment = request.form.get('comment')
+
+    workout = Workout(pushups=pushups, comment=comment, author=current_user)
+    db.session.add(workout)
+    db.session.commit()
+    flash('Your workout has been added!')
+
+    return redirect(url_for('main.user_workouts'))
+
+@main.route('/all')
+@login_required
+def user_workouts():
+    user = User.query.filter_by(email=current_user.email).first_or_404()
+    workouts = user.workouts
+    return render_template('all_workouts.html', workouts=workouts, user=user)
